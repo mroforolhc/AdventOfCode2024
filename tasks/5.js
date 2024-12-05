eb979b4278421c41d91141c02f89cdeee063b144
Markov <mroforolhc@gmail.com>
2024-12-05 22:58:19 +0300

    add day 5.2

--- tasks/5.js
+++ tasks/5.js
@@ -2,10 +2,10 @@ import getInput from "../get-input.js";
 
 async function task1() {
     const input = await getInput(5);
-    
-    let [rules, updates] = input.split('\n\n'); 
+
+    let [rules, updates] = input.split('\n\n');
     rules = rules.split('\n').map((rule) => rule.split('|').map((n) => Number(n)));
-    updates = updates.split('\n').map((update) => update.split(',').map((n) => Number(n)));    
+    updates = updates.split('\n').map((update) => update.split(',').map((n) => Number(n)));
     updates.pop();
 
     const correctUpdates = [];
@@ -25,7 +25,7 @@ async function task1() {
 
         if (!isInvalid) correctUpdates.push(update);
     })
-   
+
     let sum = 0;
     correctUpdates.forEach((update) => {
         sum += update[(update.length - 1) / 2];
@@ -36,12 +36,47 @@ async function task1() {
 
 async function task2() {
     const input = await getInput(5);
-    
-    let [rules, updates] = input.split('\n\n'); 
+
+    let [rules, updates] = input.split('\n\n');
     rules = rules.split('\n').map((rule) => rule.split('|').map((n) => Number(n)));
-    updates = updates.split('\n').map((update) => update.split(',').map((n) => Number(n)));    
+    updates = updates.split('\n').map((update) => update.split(',').map((n) => Number(n)));
     updates.pop();
 
+    const incorrectUpdates = [];
+
+    updates.forEach((update) => {
+        let isInvalid = false;
+        const relatedRules = [];
+
+        rules.forEach((rule) => {
+            const leftRuleIndex = update.findIndex((page) => page === rule[0]);
+            const rightRuleIndex = update.findIndex((page) => page === rule[1]);
+
+            if (~leftRuleIndex && ~rightRuleIndex) {
+                relatedRules.push(rule);
+
+                if (leftRuleIndex > rightRuleIndex) {
+                    isInvalid = true;
+                }
+            }
+        })
+
+        if (isInvalid) incorrectUpdates.push([update, relatedRules]);
+    })
+
+    let sum = 0;
+    incorrectUpdates.forEach(([update, rules], i) => {
+        const pages = [];
+
+        update.forEach((page) => {
+            pages[rules.filter((rule) => rule[1] === page).length] = page;
+
+        })
+
+        sum += pages[(update.length - 1) / 2];
+    })
+
+    console.log(sum);
 }
 
 // task1();
